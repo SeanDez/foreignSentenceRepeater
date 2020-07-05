a595f82bca8ac77b4184725981b41dc0d68290da
Sean Dez <sd@seandezoysa.com>
2020-07-05 19:36:26 +0700

    tweak ffmpeg description step text

--- src/classes/setupWizard/InstallFfMpeg.ts
+++ src/classes/setupWizard/InstallFfMpeg.ts
@@ -11,12 +11,14 @@ export default class InstallFfMpeg extends StepsBase implements WizardSteps {
 
    protected readonly description: string = `
 
-FFmpeg is a helpful utility library for making and combining audio files. This application makes heavy use of FFmpeg during the build process. 
+FFmpeg is a helpful utility for making and combining audio files. This audio translation application makes heavy use of FFmpeg during the audio building process. 
 
-If you do not already have this library installed, please take the time to do so now. 
+If you do not already have FFmpeg installed, please take the time to install it now. 
 
-Installation options for Windows, Mac, and Linux can be found on this page: https://ffmpeg.org/download.html
+Installation options for Windows, Mac, and Linux are available, but can change. As of June 2020, this is a good guide for Ubuntu and Linux Mint installations: https://www.tecmint.com/install-ffmpeg-in-linux/
+
+Mac and Windows users: Please check Google for a good recent guide on how to install FFmpeg on your machine. 
 `
 
-   public promptMessage: string = `Press any key to continue after you have installed FFmpeg`;
+   public promptMessage: string = `Press any key to continue after you have installed FFmpeg.`;
 }
