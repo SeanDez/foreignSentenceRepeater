3295e78372a83c4c37991fd4cb8e6629127d0d5a
Sean Dez <sd@seandezoysa.com>
2020-07-13 18:55:37 +0700

    add eslint, fix globals.ts file

--- src/globals.ts
+++ src/globals.ts
@@ -1,12 +1,11 @@
-import path from "path";
+import path from 'path';
 
-export const audioParentFolderPath = path.join(__dirname, "../../../audioCourse");
+export const audioParentFolderPath = path.join(__dirname, '../../../audioCourse');
 
-export const silenceFolderPath = path.join(__dirname, "../../../silences");
+export const silenceFolderPath = path.join(__dirname, '../../../silences');
 
-export const oneSecondPause = path.join(`${silenceFolderPath}`, `1.ogg`);
-export const twoSecondPause = path.join(`${silenceFolderPath}`, `2.ogg`);
-export const threeSecondPause = path.join(`${silenceFolderPath}`, `3.ogg`);
-export const fourSecondPause = path.join(`${silenceFolderPath}`, `4.ogg`);
-export const fiveSecondPause = path.join(`${silenceFolderPath}`, `5.ogg`);
-
+export const oneSecondPause = path.join(`${silenceFolderPath}`, '1.ogg');
+export const twoSecondPause = path.join(`${silenceFolderPath}`, '2.ogg');
+export const threeSecondPause = path.join(`${silenceFolderPath}`, '3.ogg');
+export const fourSecondPause = path.join(`${silenceFolderPath}`, '4.ogg');
+export const fiveSecondPause = path.join(`${silenceFolderPath}`, '5.ogg');
